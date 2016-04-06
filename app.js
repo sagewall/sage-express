188fef45079b49473bd9e10450608625d3df7dbd
Sage Wall <sagewall@users.noreply.github.com>
2016-04-05 18:01:57 -0600

    Add projects route.

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ var autoprefixer = require('express-autoprefixer');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
+var projects = require('./routes/projects');
 
 var app = express();
 app.set('env', 'development');
@@ -36,6 +37,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/users', users);
+app.use('/projects', projects);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
