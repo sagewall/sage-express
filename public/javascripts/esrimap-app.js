5f908d86fe418045b94c5a3127eb96cdae58fead
Sage Wall <sagewall@users.noreply.github.com>
2016-04-09 14:24:00 -0600

    Replace " with '

--- public/javascripts/esrimap-app.js
+++ public/javascripts/esrimap-app.js
@@ -1,9 +1,9 @@
 require([
-  "esri/map",
-  "esri/arcgis/utils",
-  "dojo/domReady!"
+  'esri/map',
+  'esri/arcgis/utils',
+  'dojo/domReady!'
 ], function(Map, arcgisUtils){
-  arcgisUtils.createMap("a3ecc21f773a4e1aab73ae5aa5f015ae", "mapDiv").then(function (response) {
+  arcgisUtils.createMap('a3ecc21f773a4e1aab73ae5aa5f015ae', 'mapDiv').then(function (response) {
     var map = response.map;
   });
 });
