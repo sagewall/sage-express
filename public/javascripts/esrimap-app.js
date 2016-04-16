267b4e80d55f17f63d4ee768dfec068b6afd4a13
Sage Wall <sagewall@users.noreply.github.com>
2016-04-15 21:03:40 -0600

    Add Time Slider.

--- public/javascripts/esrimap-app.js
+++ public/javascripts/esrimap-app.js
@@ -1,14 +1,18 @@
 require([
   'esri/map',
   'esri/arcgis/utils',
-  "esri/dijit/Search",
-  "esri/dijit/LayerList",
+  'esri/dijit/Search',
+  'esri/dijit/LayerList',
+  'esri/TimeExtent',
+  'esri/dijit/TimeSlider',
   'dojo/domReady!'
 ], function (Map,
              arcgisUtils,
              Search,
-             LayerList) {
-  arcgisUtils.createMap('d2a52f9eb6db4bc884f0d9f093bbf4da', 'mapDiv').then(function (response) {
+             LayerList,
+             TimeExtent,
+             TimeSlider) {
+  arcgisUtils.createMap('3e9b8ac2ed2244bd812f639fe62a5ccb', 'mapDiv').then(function (response) {
     var map = response.map;
     var searchOptions = {
       map: map
@@ -24,5 +28,21 @@ require([
       layers: []
     },"layersDiv");
     layerList.startup();
+
+
+    var timeSlider = new TimeSlider({
+      style: 'width: 100%;'
+    }, 'timeSliderDiv');
+    var snowLayer = map.getLayer('NDFD_SnowFall_5192');
+    var timeExtent = snowLayer.timeInfo.timeExtent;
+    $('.start-time').text(timeExtent.startTime.toLocaleString());
+    $('.end-time').text(timeExtent.endTime.toLocaleString());
+    timeSlider.createTimeStopsByTimeInterval(timeExtent, 6, 'esriTimeUnitsHours');
+    timeSlider.startup();
+    map.setTimeSlider(timeSlider);
+    
+    timeSlider.on('time-extent-change', function(){
+      $('.current-time').text(timeSlider.getCurrentTimeExtent().endTime.toLocaleString())
+    });
   });
 });
