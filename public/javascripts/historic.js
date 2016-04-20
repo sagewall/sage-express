5a7a3a8fedcc76678d73ea80bbe5a639d5f2b6ce
Sage Wall <sagewall@users.noreply.github.com>
2016-04-20 08:58:03 -0600

    Update mapbox routes.

--- public/javascripts/historic.js
+++ public/javascripts/historic.js
@@ -1,4 +1,21 @@
-L.mapbox.accessToken = 'pk.eyJ1Ijoic2FnZXdhbGwiLCJhIjoiMjRhNDExZWMwY2M1NzFlOTYxZWJjNjRiZTBhZGQ2NDEifQ.85AyZco3_blL_yZ0dv3Bog';
+mapboxgl.accessToken = 'pk.eyJ1Ijoic2FnZXdhbGwiLCJhIjoiMjRhNDExZWMwY2M1NzFlOTYxZWJjNjRiZTBhZGQ2NDEifQ.85AyZco3_blL_yZ0dv3Bog';
 
-var map = L.mapbox.map('mapDiv', 'mapbox.pirates')
-  .setView([39.7555, -105.2211], 11);
+var map = new mapboxgl.Map({
+  container: 'mapDiv',
+  style: 'mapbox://styles/sagewall/cin8vj3is0000b4nos3pormeg',
+  center: [-105.2211, 39.7555],
+  zoom: 11
+});
+
+map.on('click', function(e){
+  console.log('click');
+  var features = map.queryRenderedFeatures(e.point);
+  if(!features.length){
+    return;
+  }
+  var feature = features[0];
+  var popup = new mapboxgl.Popup()
+    .setLngLat(feature.geometry.coordinates)
+    .setHTML(feature.properties.PopupInfo)
+    .addTo(map);
+});
