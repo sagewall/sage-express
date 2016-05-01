c5f2930bb2b9afaddaa57a787eac83389e890f18
Sage Wall <sagewall@users.noreply.github.com>
2016-04-30 19:14:06 -0600

    Add description to side column.

--- public/javascripts/historic.js
+++ public/javascripts/historic.js
@@ -1,25 +1,31 @@
 L.mapbox.accessToken = 'pk.eyJ1Ijoic2FnZXdhbGwiLCJhIjoiMjRhNDExZWMwY2M1NzFlOTYxZWJjNjRiZTBhZGQ2NDEifQ.85AyZco3_blL_yZ0dv3Bog';
 
-var map = L.mapbox.map('mapDiv', 'mapbox.pirates')
+var map = L.mapbox.map('mapDiv', 'mapbox.streets')
   .setView([39.7555, -105.2211], 11);
 
-var featureGroup = L.featureGroup().addTo(map);
+var drawingGroup = L.featureGroup().addTo(map);
 
 new L.Control.Draw({
   edit: {
-    featureGroup: featureGroup
+    featureGroup: drawingGroup
   }
 }).addTo(map);
 
 map.on('draw:created', function(e) {
-  featureGroup.addLayer(e.layer);
+  drawingGroup.addLayer(e.layer);
 });
 
 
 $.getJSON('api/historic-places', function(data){
   console.log(data);
   $.each(data, function(index, value){
-    L.geoJson(value).addTo(map).bindPopup(value['properties']['description']);
+    L.geoJson(value)
+      .bindPopup(value.properties.name)
+      .on('click', showDescription)
+      .addTo(map);
   });
-
 });
+
+function showDescription(e){
+  $('#description').html(e.layer.feature.properties.description);
+}
