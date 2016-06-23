54f31c2e0f7483ba5e6102e084f8cff61a58cb41
Sage Wall <sagewall@users.noreply.github.com>
2016-06-22 20:09:29 -0600

    Add simple popup to parcel layer

--- public/javascripts/parcels.js
+++ public/javascripts/parcels.js
@@ -1,6 +1,29 @@
-L.mapbox.accessToken = 'pk.eyJ1Ijoic2FnZXdhbGwiLCJhIjoiMjRhNDExZWMwY2M1NzFlOTYxZWJjNjRiZTBhZGQ2NDEifQ.85AyZco3_blL_yZ0dv3Bog';
+mapboxgl.accessToken = 'pk.eyJ1Ijoic2FnZXdhbGwiLCJhIjoiMjRhNDExZWMwY2M1NzFlOTYxZWJjNjRiZTBhZGQ2NDEifQ.85AyZco3_blL_yZ0dv3Bog';
 
-var map = L.mapbox.map('mapDiv')
- .setView([39.74, -104.99], 15);
+var map = new mapboxgl.Map({
+  container: 'mapDiv',
+  style: 'mapbox://styles/sagewall/ciphm32fr000ybjm5rhsz8hmx',
+  center: [-104.99, 39.74],
+  zoom: 15
+});
 
-L.mapbox.styleLayer('mapbox://styles/sagewall/ciphm32fr000ybjm5rhsz8hmx').addTo(map);
+console.log(map.queryRenderedFeatures());
+
+map.on('click', function (e) {
+  var features = map.queryRenderedFeatures(e.point, { layers: ['parcels'] });
+  if (!features.length) {
+    console.log('No parcels found');
+  } else {
+    $.each(features, function(index, value){
+      new mapboxgl.Popup()
+        .setLngLat(map.unproject(e.point))
+        .setHTML(value.properties.PIN)
+        .addTo(map);
+    });
+  }
+});
+
+map.on('mousemove', function (e) {
+  var features = map.queryRenderedFeatures(e.point, { layers: ['parcels'] });
+  map.getCanvas().style.cursor = (features.length) ? 'pointer' : '';
+});
