e1c6a3cd2df05797e860f7ec453ce5dc4e102ef3
Sage Wall <sagewall@users.noreply.github.com>
2016-09-20 19:30:42 -0600

    Switch Forecast.io to DarkSky.net api

--- public/javascripts/weather.js
+++ public/javascripts/weather.js
@@ -3,18 +3,17 @@ $.getJSON('//api.wunderground.com/api/1f8c5d8b0f435e87/forecast/q/CO/Golden.json
   $('.wu-tomorrow').html(data.forecast.txt_forecast.forecastday[1].fcttext);
 });
 
-url = '//api.forecast.io/forecast/76e613916d6f9b5d1ee8447f2aa73bc5/39.7555,-105.2211';
+darkSkyUrl = '//api.darksky.net/forecast/76e613916d6f9b5d1ee8447f2aa73bc5/39.7555,-105.2211';
 
 $.ajax({
   dataType: "jsonp",
-  url: url,
+  url: darkSkyUrl,
   success: displayForecastIO
 });
 
 function displayForecastIO(data) {
-  console.log(data);
-  $('.io-currently-summary').html(data.currently.summary);
-  $('.io-minutely-summary').html(data.minutely.summary);
-  $('.io-hourly-summary').html(data.hourly.summary);
-  $('.io-daily-summary').html(data.daily.summary);
+  $('.ds-currently-summary').html(data.currently.summary);
+  $('.ds-minutely-summary').html(data.minutely.summary);
+  $('.ds-hourly-summary').html(data.hourly.summary);
+  $('.ds-daily-summary').html(data.daily.summary);
 }
